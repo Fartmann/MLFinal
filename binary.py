c4b45a00016fa12798d9eedebca9e3a845d14343
Abam <adamsatyshev10@gmail.com>
2025-05-21 14:46:58 +0500

    changed binary.py

--- binary.py
+++ binary.py
@@ -2,13 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.naive_bayes import GaussianNB
-from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 import joblib
 
@@ -49,7 +43,7 @@ for name, model in models.items():
     print("F1 Score:", f1_score(y_test, preds))
     print("ROC-AUC:", roc_auc_score(y_test, proba))
 
-best_model = GaussianNB()
+best_model = RandomForestClassifier()
 best_model.fit(X_train, y_train)
 joblib.dump(best_model, "heart_model.pkl")
 joblib.dump(scaler, "scaler.pkl")
